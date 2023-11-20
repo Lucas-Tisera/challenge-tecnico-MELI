d6f1d65ee583436feedf52b37aed7c392eb55803
Lucas Tisera <lucastisera02@gmail.com>
2023-11-20 00:22:43 -0300

    Fix - modificaciones para mejorar SEO

--- cliente/src/screens/HomeScreen.jsx
+++ cliente/src/screens/HomeScreen.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export const HomeScreen = () => {
   return (
-    <section>
-      <title className='list_screen_error_container'><h1 className='list_screen_error_message'>Ingrese algun valor de busqueda para comenzar</h1></title> 
-    </section>
+      <section className='list_screen_error_container'>
+        <h1 className='list_screen_error_title'>Bienvenido a Mercado Libre</h1>
+        <h2 className='list_screen_error_message'>Ingrese algun valor de busqueda para comenzar</h2>
+        <p>
+          Esta aplicación esta basada en un test de Mercado Libre, la cual consiste en crear una aplicación que permita buscar productos y ver sus detalles.
+          La aplicación esta desarrollada en React JS, con el uso de Hooks, Context API, React Router, entre otras librerias.
+        </p>
+      </section> 
   )
 }
