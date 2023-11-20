2977a9dcfa06c29f4e9ce3d2e7762e417c43c6c0
Lucas Tisera <lucastisera02@gmail.com>
2023-11-20 00:33:32 -0300

    Feat - homeScreen, descripcion del proyecto

--- cliente/src/screens/HomeScreen.jsx
+++ cliente/src/screens/HomeScreen.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const HomeScreen = () => {
   return (
-      <section className='list_screen_error_container'>
-        <h1 className='list_screen_error_title'>Bienvenido a Mercado Libre</h1>
-        <h2 className='list_screen_error_message'>Ingrese algun valor de busqueda para comenzar</h2>
-        <p>
+      <section className='home_screen_container'>
+        <h1 className='home_screen_title'>Bienvenido a Mercado Libre</h1>
+        <h2 className='home_screen_subtitle'>Ingrese algun valor de busqueda para comenzar</h2>
+        <p className='home_screen_paragraph'>
           Esta aplicación esta basada en un test de Mercado Libre, la cual consiste en crear una aplicación que permita buscar productos y ver sus detalles.
           La aplicación esta desarrollada en React JS, con el uso de Hooks, Context API, React Router, entre otras librerias.
         </p>
