6310ad9bac4ff4ebf9404c14fbc3ef160c9d66df
Lucas Tisera <lucastisera02@gmail.com>
2023-11-19 21:25:14 -0300

    Fix - rewritePath

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,14 +1,13 @@
 const { Router } = require('express');
 
 const itemsRouter = require('./items');
-const proxyMiddleware = require('../middleware/routerMiddleware');
 
 const router = Router();
 
 
 
 // Configuro los routers que se utilizarán para la aplicación
-router.use('/items', proxyMiddleware, itemsRouter);
+router.use('/items', itemsRouter);
 
 
 module.exports = router;
